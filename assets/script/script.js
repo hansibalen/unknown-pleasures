ef1d6d013fed52df10fdf5a8b4e6b1f4c3a0f4ec
hnsblnbrg <hansibrahimasi@gmail.com>
2021-07-31 00:56:17 +0200

    Adjust wave lines

--- assets/script/script.js
+++ assets/script/script.js
@@ -24,21 +24,35 @@ let mx = (xMin + xMax) / 2;
 ctx.fillStyle = "black";
 ctx.strokeStyle = "white";
 
-//Two nested loops to iterate each line one by one
+//Two nested loops to iterate each line of the cover one by one
 for (let i = 0; i < nLines; i++) {
   ctx.beginPath();
+  let nModes = randInt(1, 4);
   // Generate random parameters for the line's normal distribution
-  let mu = randNormal(mx, 50);
-  let sigma = randNormal(30, 30);
-  for (let j = 0; j < nPoints; j++) {
-    x = x + dx;
-    ctx.lineTo(x, y - 1000 * normalPDF(x, mu, sigma));
+  let mus = [];
+  let sigmas = [];
+  for (let j = 0; j < nModes; j++) {
+    mus[j] = randNormal(mx, 100);
+    sigmas[j] = randNormal(24, 30);
   }
-  // Cover previous lines
+  for (let k = 0; k < nPoints; k++) {
+    x = x + dx;
+    let noise = 0;
+    for (let l = 0; l < nModes; l++) {
+      noise += normalPDF(x, mus[l], sigmas[l]);
+    }
+    //You can add the line commented below for a glitchy look
+    // ctx.lineTo(x, y - 1000 * noise);
+
+    //Adjust wave lines similar to the album cover
+    var yy = y - (500 + Math.random() * 200) * noise + Math.random();
+    ctx.lineTo(x, yy);
+  }
+  //Cover pixels of previous line
   ctx.fill();
-  // Draw current line
+  //Draw current line
   ctx.stroke();
-  // Go to next line
+  //Move to next line
   x = xMin;
   y = y + dy;
   ctx.moveTo(x, y);
@@ -47,6 +61,10 @@ for (let i = 0; i < nLines; i++) {
 ctx.lineWidth = 1.2;
 ctx.strokeStyle = "white";
 ctx.stroke();
+
+function randInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 function rand(min, max) {
   return Math.random() * (max - min) + min;
